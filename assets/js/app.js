cb0c62e08209d5ad1e253fe35c1de88382e0bc07
IKOAWAJI AWAJIOYEM MIRACLE <drawajioyem@gmail.com>
2020-07-29 06:42:26 -0500

    Add files via upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,10 @@ function myImage() {
 
     document.getElementById("image-upload").src = "assets/img/icons/uploaded.png";
 }
+
+// side menu
+
+document.getElementById("toggle").addEventListener("click", function() {
+
+    $("#side-nav").slideToggle();
+});
